75b6bef32926907bf695564b3f543478e82df190
Aditya <aditya01082003adi@gmail.com>
2026-02-04 17:13:11 +0530

    asdfgr

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/app/layout/Header";
+import Footer from "@/app/layout/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,10 +21,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header />
         {children}
+        <Footer/>
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+"use client"
+
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      Harsha Jasrotia
+      harsha
     </div>
   )
 }
